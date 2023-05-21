36d2e7be8da12973ed17c9fb6198aacd3bc50e62
sinswitch <sin6708k@gmail.com>
2023-05-21 19:51:50 +0900

    Add data models and unit-test for them

---
+++ app/src/main/java/com/example/catchingbus/model/Bus.kt
@@ -0,0 +1,23 @@
+package com.example.catchingbus.model
+
+import com.example.catchingbus.model.json.BusByStationJsonApi
+import com.example.catchingbus.model.json.BusJsonApi
+
+data class Bus(
+    val id: String,
+    val name: String,
+    val intervalTime: Int,
+    val type: String
+) {
+    companion object {
+        fun search(station: Station): List<Bus> {
+            val busByStationJsons = BusByStationJsonApi.request("22", station.id)
+            val busJsons = busByStationJsons.map {
+                BusJsonApi.request("22", it.routeid).first()
+            }
+            return busJsons.map {
+                Bus(it.routeid, it.routeno, it.intervaltime, it.routetp)
+            }
+        }
+    }
+}
