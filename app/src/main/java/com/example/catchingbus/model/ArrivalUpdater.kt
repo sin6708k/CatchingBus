d039af6c9f2aa178a10f9031b0a10b8c10d549ea
sinswitch <sin6708k@gmail.com>
2023-05-24 00:24:16 +0900

    Add ArrivalUpdaterLauncher

--- app/src/main/java/com/example/catchingbus/model/ArrivalUpdater.kt
+++ app/src/main/java/com/example/catchingbus/model/ArrivalUpdater.kt
@@ -1,7 +1,7 @@
 package com.example.catchingbus.model
 
 import kotlinx.coroutines.channels.Channel
-import kotlin.properties.Delegates
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 class ArrivalUpdater(
     private val station: Station,
@@ -10,17 +10,13 @@ class ArrivalUpdater(
 
 ): Runnable(alarm?.activeSchedules ?: listOf()) {
 
-    val latest by Delegates.observable(ArrivalInfo(station, bus, listOf())) { _, _, _ ->
-        onLatestListener?.also { it() }
-    }
+    val latest by lazy { MutableSharedFlow<ArrivalInfo>() }
     val alarmMessage by lazy { Channel<AlarmMessage>() }
-
-    var onLatestListener: (() -> Unit)? = null
 
     override suspend fun run(): RepeatCommand {
         val new = ArrivalInfo.search(station, bus)
 
-        alarm?.also { alarm ->
+        if (alarm != null) {
             if (new.remainingTimes.first() < alarm.time) {
                 alarmMessage.send(AlarmMessage(new))
             }
