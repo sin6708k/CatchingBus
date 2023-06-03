2e2f09bbfb62dd7f1fe270280ffb3e2de314161d
sinswitch <sin6708k@gmail.com>
2023-06-03 20:46:17 +0900

    Add comments for FavoriteViewModel

--- app/src/main/java/com/example/catchingbus/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/example/catchingbus/viewmodel/FavoriteViewModel.kt
@@ -19,10 +19,13 @@ class FavoriteViewModel: ViewModel() {
         const val TAG = "problem"
     }
 
+    // 이 field의 값이 바뀔 때마다 View에서 보여주는 Favorite들을 갱신해야 한다.
     val favorites: LiveData<List<Favorite>> = FavoriteRepo.data.asLiveData()
 
+    // View에서 Favorite를 선택할 때마다 이 field를 그 Favorite로 설정해야 한다.
     val selectedFavorite = MutableLiveData<Favorite?>()
 
+    // 이 field의 값이 바뀔 때마다 View에서 보여주는 Schedule들을 갱신해야 한다.
     val schedules: LiveData<List<Schedule>> get() = _schedules
     private val _schedules = MutableLiveData(listOf<Schedule>())
 
@@ -49,6 +52,7 @@ class FavoriteViewModel: ViewModel() {
         }
     }
 
+    // View에서 Favorite 삭제 버튼을 누를 때마다 이 function을 호출해야 한다.
     fun removeFavorite(favorite: Favorite) = viewModelScope.launch {
         FavoriteRepo.remove(favorite)
     }
@@ -57,6 +61,7 @@ class FavoriteViewModel: ViewModel() {
         _schedules.value = favorite?.alarmActiveSchedules
     }
 
+    // View에서 Schedule 등록 버튼을 누를 때마다 이 function을 호출해야 한다.
     fun addSchedule(startTime: LocalTime, endTime: LocalTime) = viewModelScope.launch {
         selectedFavorite.value?.let {
             val schedule = Schedule(startTime, endTime)
@@ -66,6 +71,7 @@ class FavoriteViewModel: ViewModel() {
         }
     }
 
+    // View에서 Schedule 삭제 버튼을 누를 때마다 이 function을 호출해야 한다.
     fun removeSchedule(schedule: Schedule) = viewModelScope.launch {
         selectedFavorite.value?.let {
             it.alarmActiveSchedules.remove(schedule)
